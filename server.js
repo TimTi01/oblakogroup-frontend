462a27a83a262ec18c3e2d7d80eaba5f9a3690d6
Artem Fadeev <TimF1234@yandex.ru>
2022-07-03 15:19:59 +0300

    added fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/angular-todo-oblakogroup'));
-
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/angular-todo-oblakogroup/'});
+app.use(express.static(__dirname + '/dist/angular-todo-oblakogroup/'));
+app.get('/*', function(req,res) {
+    res.sendFile(__dirname+'/dist/angular-todo-oblakogroup/index.html');
 });
-
 app.listen(process.env.PORT || 8080);
